2553bf4c534b48e4448c2fd680f519c4e78d23b2
berunda <jonberunda@gmail.com>
2025-01-05 20:57:23 -0500

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/berunda/dreampicai
+
+go 1.23.2
+
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router := chi.NewMux()
+
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("application running", "port", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
